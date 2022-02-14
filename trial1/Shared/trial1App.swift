b52497cbbf4b3f8efadcd2765548bc35d08057c9
Tomás Fernández Bouvier <tomasfbouvier@gmail.com>
2022-02-14 12:21:09 +0100

    added Data_exploration.ipynb

---
+++ trial1/Shared/trial1App.swift
@@ -0,0 +1,17 @@
+//
+//  trial1App.swift
+//  Shared
+//
+//  Created by Tomás Fernández Bouvier on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct trial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
