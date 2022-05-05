be9bb63cbbdb2f6e28f7177137431e5339f62081
Tomás Fernández Bouvier <tomasfbouvier@gmail.com>
2022-05-05 11:36:37 +0200

    reformulated TCT

--- TCT.py
+++ TCT.py
@@ -9,7 +9,7 @@ Created on Fri Apr  1 14:21:05 2022
 from sklearn.cluster import DBSCAN
 import numpy as np
 from create_sets import create_sets
-
+import matplotlib.pyplot as plt
 
 def time_consistency_test(station):
     """
@@ -21,17 +21,19 @@ def time_consistency_test(station):
         - predict: closure functiuon that evaluates wether a given point belongs to a cluster or not
     """
     
-    x, f= create_sets(station)
-    y=f(x)
+    xs, f= create_sets(station)
+    ys= f(xs)
 
-    X = np.array([x,y])
-    X= X.T
 
-    y_steps= [y[i+1]-y[i] for i in range(len(y)-1)]
-    x_steps= [x[i+1]-x[i] for i in range(len(x)-1)]
+    y_steps= [abs(ys[i+1]-ys[i]) for i in range(len(ys)-1)]
+    x_steps= [abs(xs[i+1]-xs[i]) for i in range(len(xs)-1)]
+    
+    
+    scale_x= np.mean(x_steps)
+    scale_y= np.mean(y_steps)
 
-    scaling_factor_y=abs(max(X[:,1])- min(X[:,1]))*10
-    scaling_factor_x=max(X[:,0])- min(X[:,0])
+
+
 
    # X[:,0]/=scaling_factor_x
    # X[:,1]/=scaling_factor_y
@@ -49,86 +51,49 @@ def time_consistency_test(station):
                 - Turn it into computationally efficient. Remove points for calculation
         """
         
-        X_aux=np.array([[x,y]])
-        X_aux[:,0]/=scaling_factor_x
-        X_aux[:,1]/=scaling_factor_y
         
-        X_new=np.append(X,X_aux, axis=0)
+        i= np.where(x==xs)[0]
         
-        labels = DBSCAN(eps=0.01,min_samples=5, metric='euclidean').fit_predict(X_new)
+        ys[i]=y
         
+        #print(i)
+        sub_xs= [xs[j] for j in range(len(xs)) if xs[j]>xs[i]-100*scale_x and xs[j]<xs[i]+100*scale_x]
+        sub_ys= [ys[j] for j in range(len(xs)) if xs[j]>xs[i]-100*scale_x and xs[j]<xs[i]+100*scale_x]
         
-        del(X_new)
+        #print(sub_xs, sub_ys)
         
-        if(labels[-1]==-1):
+        i= np.where(x==sub_xs)[0]
+        
+        sub_xs/= scale_x
+        sub_ys/= scale_y
+        
+        X=np.array([sub_xs, sub_ys]).T
+        
+        labels = DBSCAN(eps=params[0],min_samples=params[1], metric='euclidean').fit_predict(X)
+
+        del(X)
+        
+        if(labels[i]==-1):
             return True
         else:
             return False
     return predict
 
-import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
 
-from statsmodels.tsa.ar_model import ar_select_order
-from scipy.stats import gaussian_kde
-def AR_model(station):
-    
-    
-    xs, f= create_sets(station)
-    y= f(xs)
+model= time_consistency_test(4201)
+xs,f= create_sets(4201)
 
-    #y=  [y[i+1]-y[i] for i in range(len(y)-1)] 
-    
-    #X = np.array([x[1:],y])
-    
-    #X= X.T
-    
-    mod = ar_select_order(y, maxlag=13)
-    #print('ORDER:', arma_order_select_ic(y, max_ar=4, max_ma=2))
-
-    ar_model = ARIMA(endog=y, order=(mod.ar_lags,0,0),  trend='n').fit()
-    
-   # print('lags: %s' % mod.ar_lags)
-    #print(print('Coefficients: %s' % ar_model.params))
-    
-    #plt.plot(xs,y, 'b.')
-    
-    pred=ar_model.predict(start=0, end=(len(y)-1), dynamic=False)
-    
-
-    #plt.plot(xs[1:],pred[1:],'r.')
-    
-
-    #print(ar_model.summary())
-   # plt.figure()
-    
-    diffs=pred[1:]-y[1:]
-    kde=gaussian_kde(diffs)
-   # xss= np.linspace(min(diffs),max(diffs),10000 )
-   # plt.plot(xss, kde(xss))
-    
-    def evaluate_point(x, y, thr):
-        
-        i= np.where(x==xs)[0][0]
-        diff= y-ar_model.predict(start=i, end=i, dynamic=False)
-        if (kde(diff)<thr):
-            return True
-        else:
-            return False
-    return evaluate_point
+ys=f(xs)
 
 
-    # TODO: Now that I have the way to build a model implement the subsequent test
-    # TODO: switch to datetimes
-    return 
 
+from time import time
 
-xs, f= create_sets(4207)
-model=AR_model(4207)
-"""
-for x in xs:
-    if model(x, f(x), 0.005):
-        plt.plot(x, f(x), 'r.')
+start= time()
+for i in range(len(xs)):
+    if model(xs[i],ys[i], [4., 2]):
+        plt.plot(xs[i],ys[i], 'r.')
     else:
-        plt.plot(x, f(x), 'b.')
-"""
+        plt.plot(xs[i],ys[i], 'b.')
+        
+print(time()-start)
